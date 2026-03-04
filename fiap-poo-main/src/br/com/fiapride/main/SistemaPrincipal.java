88e8e9e59ee779d90c233ef059cd7ffd66f68fca
eduabreuu <129406498+eduabreuu@users.noreply.github.com>
2026-03-04 19:32:03 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- fiap-poo-main/src/br/com/fiapride/main/SistemaPrincipal.java
+++ fiap-poo-main/src/br/com/fiapride/main/SistemaPrincipal.java
@@ -3,8 +3,11 @@ package br.com.fiapride.main;
 import br.com.fiapride.model.Camera;
 import br.com.fiapride.model.Passageiro;
 
+import java.util.Scanner;
+
 public class SistemaPrincipal {
     public static void main(String[] args) throws InterruptedException {
+        Scanner leitura = new Scanner(System.in);
 //        Passageiro passageiro1 = new Passageiro();
 //        passageiro1.nome = "Jurandir";
 //        passageiro1.saldo = 70.66;
@@ -17,8 +20,15 @@ public class SistemaPrincipal {
 //        System.out.println("--- Sistema FiapRide ---");
 //        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
 //        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
-        Camera camera1 = new Camera(720,500,"Wifi");
 
-        System.out.println(camera1);
+        Camera camera1 = new Camera(720,500,"Wifi");
+        camera1.comecarFilmagem();
+
+        System.out.println("Deseja liberar espaço?(S/N): ");
+        String opcao = leitura.nextLine().toUpperCase();
+        if(opcao.equals("S")){
+            camera1.limparHd();
+        }
+
     }
 }
--- fiap-poo-main/src/br/com/fiapride/model/Camera.java
+++ fiap-poo-main/src/br/com/fiapride/model/Camera.java
@@ -9,10 +9,27 @@ public class Camera {
 
 
     public Camera(int resolucao,int capacidade, String conectividade){
-
         this.resolucao = resolucao;
         this.capacidade = capacidade;
         this.conectividade = conectividade;
+    }
+
+    public void limparHd(){
+        System.out.println("Limpando nuvem...");
+        this.capacidade = 500;
+        System.out.println("Capacidade Atual: " + this.capacidade);
+    }
+
+    public void comecarFilmagem() throws InterruptedException {
+        System.out.println("Começando a filmagem...");
+
+        while (this.capacidade > 0){
+            System.out.println("Filmando... Capacidade atual: " + this.capacidade);
+            Thread.sleep(10);
+            this.capacidade -= 1;
+        }
+        System.out.println("Capacidade Atual: " + this.capacidade);
+
     }
 
     @Override
